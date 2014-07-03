abae5309631c922cbca0d307651ce9266faa31c6
vntzy <vntzyv@gmail.com>
2014-07-03 14:46:23 +0300

    add lib folder and modify AsciiPlayer.java

--- AsciiArtViewer/src/com/hackbulgaira/asciiart/players/AsciiPlayer.java
+++ AsciiArtViewer/src/com/hackbulgaira/asciiart/players/AsciiPlayer.java
@@ -4,8 +4,11 @@ import java.io.File;
 
 public abstract class AsciiPlayer {
     private final static int columns = 140;
+    private File sourceFile;
     
-    AsciiPlayer(File file){}
+    AsciiPlayer(File file){
+        sourceFile = file;
+    }
     
     public void play(){}
 }
